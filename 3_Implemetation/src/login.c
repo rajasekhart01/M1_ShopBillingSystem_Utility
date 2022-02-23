dc210e87f17bf69ada031ba5c0055f57756632aa
Raja Sekhar Thimmarayappagari <98815258+rajasekhart01@users.noreply.github.com>
2022-02-23 20:09:35 +0530

    Update login.c

--- 3_Implemetation/src/login.c
+++ 3_Implemetation/src/login.c
@@ -1,26 +1,15 @@
 #include "bill.h"
 
-    void quantity()
+void login()
 {
-    printf("\n\t***Enter 0 for none****");
+    printf("administrator: \n");
+	scanf("%s",name);
 
-	printf("\n no. of Teas \n");
-	scanf("%d", &teaQTY);
-
-	printf(" no. of SpeacialTea \n");
-	scanf("%d", &specialteaQTY);
-
-	printf(" no. of coffees\n");
-	scanf("%d", &coffeeQTY);
-
-	printf(" no. of buscuits\n");
-	scanf("%d", &buscuitQTY);
-
-	printf(" no. of buns \n");
-	scanf("%d", &bunQTY);
-
-	printf(" no. of Milks \n");
-	scanf("%d", &milkQTY);
+	printf("Customer's Name: \n");
+	scanf("%s",cname);
+		printf("Date (dd/mm/yyyy)\n");
+	scanf("%s",date);
 	
-    billgeneration();
+    quantity();
+    
 }
