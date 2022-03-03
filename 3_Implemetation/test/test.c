034fdf4b6d337a060664f07f1e7c5036c764057e
Raja Sekhar Thimmarayappagari <98815258+rajasekhart01@users.noreply.github.com>
2022-03-03 23:00:50 +0530

    Update test.c

--- 3_Implemetation/test/test.c
+++ 3_Implemetation/test/test.c
@@ -1,1 +1,30 @@
-# yet to be done
+#include "bill.h"
+#include "unity.h"
+
+void setUp()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+void test_login (void);
+
+
+int main()
+{
+    UNITY_BEGIN();
+
+    RUN_TEST(login);
+
+    return UNITY_END();
+}
+
+void test_login (void)
+{   
+    TEST_ASSERT_EQUAL(-1,login());
+}
+
